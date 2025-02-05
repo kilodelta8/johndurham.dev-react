7198e124e01041a828d87d9b11d730e4c96635a6
kilodelta8 <jdurham937@gmail.com>
2025-02-05 17:31:24 -0500

    Added SS

---
+++ src/components/Home.tsx
@@ -0,0 +1,37 @@
+
+
+const Home = () => {
+  return (
+    <>
+        <div className="container">
+            <br/>
+            <br/>
+            <h1>Hi! I'm John!</h1>
+            <br/>
+            <br/>
+            <p>A Software Developer from Dayton, OH.</p>
+            <br/>
+            <p id="code">Let's code -&gt;</p>
+            <p>
+            <a href="https://www.linkedin.com/in/johnsdurham/" className="fa fa-linkedin">  </a>
+            <a href="https://github.com/kilodelta8" className="fa fa-github">  </a>
+            <a href="https://twitter.com/KiloDelta8" className="fa fa-twitter">  </a>
+            </p>
+            <br/>
+            <br/>
+            <p id="about">I am an aspiring software engineer in Dayton, OH.  I am currently attending Sinclair Community College
+                for an Associates of Science in Software Development.  I am a graduate of LaunchCode's Full Stack Web Development Bootcamp.
+                I am well versed in C++ and Java with strong experience in Python and C#.
+                I am always coding and working on a project(s) from a home Google Wall Calendar & Server to automating my garden/plants to
+                building tube amps and radios (Ham enthusiast, I). I am also a musician writing, recording and (covid pending) performing.
+                I hope to enter the software field working on mission critical systems, embedded systems, operating systems, and systems in general.
+                There is no project or problem I would not enthusiastically take on.
+                I am also always excited for meetups, collaboration on projects, hackathons, etc.
+            </p>
+        </div>
+        <a href="#top" id="top-link">Back To The Top</a>
+    </>
+  )
+}
+
+export default Home

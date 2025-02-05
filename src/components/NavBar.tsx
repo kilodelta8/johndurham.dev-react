486c1f4e8e0ffb837e429ebe44ae446f5684cfa8
kilodelta8 <jdurham937@gmail.com>
2025-02-05 04:37:10 -0500

    Init commit

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,17 @@
+
+
+const NavBar = () => {
+  return (
+    <>
+        <nav id="top">
+            <ul>
+                <li><a href="/projects.html">Projects<span className="material-icons-outlined">code</span></a></li>
+                <li><a href="#about">About<span className="material-icons-outlined">info</span></a></li>
+                <li><a href="/hobbies.html">Hobbies<span className="material-icons-outlined">settings_accessibility</span></a></li>
+            </ul>
+        </nav>
+    </>
+  )
+}
+
+export default NavBar
